a82e2ea7ea1a5b4176a3bac6ded5ae6d4ca7df00
Paul Fritsche <paul.fritsche@gmail.com>
2015-10-04 13:21:09 +0200

    again ;-)

---
+++ core/src/main/java/org/synopia/audio/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package org.synopia.audio.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.synopia.audio.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1600;
+		config.height = 1200;
+		new LwjglApplication(new Main(), config);
+	}
+}
